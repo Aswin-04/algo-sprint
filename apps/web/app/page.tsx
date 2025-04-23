c0f2dc24eb5ab25eee85d852213169883ab2b721
Aswin-04 <e.aswin004@gmail.com>
2025-04-23 23:46:08 +0530

    Add next app and shared tailwind config package

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@repo/ui/button'
+
+export default function Home() {
+  return (
+    <div>
+      <Button className='text-primary-500 font-bold text-9xl mx-10 mt-20' appName='web'>Hi there</Button>
+    </div>
+  );
+}
